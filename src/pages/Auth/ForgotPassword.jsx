57a8fa0bd8cef87f7e35077ecc86d2c290e2e76e
Arulmozhikumar <arulmozhikumar7@gmail.com>
2024-03-26 12:10:39 +0530

    Adjusted Mobile View

--- src/pages/Auth/ForgotPassword.jsx
+++ src/pages/Auth/ForgotPassword.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 import { ResetPass } from "@assets";
 const ForgotPassword = () => {
   return (
-    <section class="bg-gray-50 dark:bg-gray-900 grid md:grid-cols-2">
+    <section class="bg-gray-50  grid md:grid-cols-2">
       <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div class="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
-          <h1 class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+        <div class="w-full p-6 bg-white rounded-lg shadow  md:mt-0 sm:max-w-md  sm:p-8">
+          <h1 class="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
             Forgot your password?
           </h1>
-          <p class="font-light text-gray-500 dark:text-gray-400">
+          <p class="font-light text-gray-500 ">
             Don't fret! Just type in your email and we will send you a code to
             reset your password!
           </p>
@@ -17,7 +17,7 @@ const ForgotPassword = () => {
             <div>
               <label
                 for="email"
-                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                class="block mb-2 text-sm font-medium text-gray-900 "
               >
                 Your email
               </label>
@@ -25,7 +25,7 @@ const ForgotPassword = () => {
                 type="email"
                 name="email"
                 id="email"
-                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-emerald-600 focus:border-emerald-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-emerald-600 focus:border-emerald-600 block w-full p-2.5 "
                 placeholder="name@company.com"
                 required=""
               />
@@ -36,18 +36,15 @@ const ForgotPassword = () => {
                   id="terms"
                   aria-describedby="terms"
                   type="checkbox"
-                  class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-emerald-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-emerald-600 dark:ring-offset-gray-800"
+                  class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-emerald-300 "
                   required=""
                 />
               </div>
               <div class="ml-3 text-sm">
-                <label
-                  for="terms"
-                  class="font-light text-gray-500 dark:text-gray-300"
-                >
+                <label for="terms" class="font-light text-gray-500 ">
                   I accept the{" "}
                   <a
-                    class="font-medium text-emerald-600 hover:underline dark:text-emerald-500"
+                    class="font-medium text-emerald-600 hover:underline "
                     href="#"
                   >
                     Terms and Conditions
@@ -57,7 +54,7 @@ const ForgotPassword = () => {
             </div>
             <button
               type="submit"
-              class="w-full text-white bg-emerald-600 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+              class="w-full text-white bg-emerald-600 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center "
             >
               Reset password
             </button>
--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 import { CNG } from "@assets";
 const Login = () => {
   return (
-    <section class="bg-gray-50 dark:bg-gray-900 grid grid-cols-1 md:grid-cols-2 w-full">
-      <div class="flex flex-col items-center w-full  py-4 mx-auto md:h-1/2 lg:py-0">
-        <div class="w-full bg-white rounded-lg shadow dark:border md:mt-8 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+    <section class="bg-gray-50  grid grid-cols-1 md:grid-cols-2 w-full">
+      <div class="flex flex-col items-center w-full px-2  py-4 mx-auto md:h-1/2 lg:py-0">
+        <div class="w-full bg-white rounded-lg shadow  md:mt-8 sm:max-w-md xl:p-0 ">
           <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 text-center md:text-2xl dark:text-white">
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 text-center md:text-2xl ">
               Sign In Now
             </h1>
             <form class="space-y-4 md:space-y-6" action="#">
               <div>
                 <label
                   for="email"
-                  class="block mb-2 text-sm font-medium text-gray-800 dark:text-white"
+                  class="block mb-2 text-sm font-medium text-gray-800 "
                 >
                   Your email
                 </label>
@@ -22,7 +22,7 @@ const Login = () => {
                   type="email"
                   name="email"
                   id="email"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-emerald-600 focus:border-emerald-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-emerald-600 focus:border-emerald-600 block w-full p-2.5"
                   placeholder="name@company.com"
                   required=""
                 />
@@ -30,7 +30,7 @@ const Login = () => {
               <div>
                 <label
                   for="password"
-                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  class="block mb-2 text-sm font-medium text-gray-900 "
                 >
                   Password
                 </label>
@@ -39,7 +39,7 @@ const Login = () => {
                   name="password"
                   id="password"
                   placeholder="••••••••"
-                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-emerald-600 focus:border-emerald-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-emerald-600 focus:border-emerald-600 block w-full p-2.5 "
                   required=""
                 />
               </div>
@@ -50,37 +50,34 @@ const Login = () => {
                       id="remember"
                       aria-describedby="remember"
                       type="checkbox"
-                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-emerald-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-emerald-600 dark:ring-offset-gray-800"
+                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-emerald-300 "
                       required=""
                     />
                   </div>
                   <div class="ml-3 text-sm">
-                    <label
-                      for="remember"
-                      class="text-gray-500 dark:text-gray-300"
-                    >
+                    <label for="remember" class="text-gray-500 ">
                       Remember me
                     </label>
                   </div>
                 </div>
                 <Link
                   to="/forgot-password"
-                  class="text-sm font-medium text-emerald-600 hover:underline dark:text-emerald-500"
+                  class="text-sm font-medium text-emerald-600 hover:underline "
                 >
                   Forgot password?
                 </Link>
               </div>
               <button
                 type="submit"
-                class="w-full text-white bg-emerald-600 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-emerald-600 dark:hover:bg-emerald-700 dark:focus:ring-emerald-800"
+                class="w-full text-white bg-emerald-600 hover:bg-emerald-700 focus:ring-4 focus:outline-none focus:ring-emerald-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
               >
                 Sign in
               </button>
-              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+              <p class="text-sm font-light text-gray-500 ">
                 New here?{" "}
                 <Link
                   to="/Register"
-                  class="font-medium text-emerald-600 hover:underline dark:text-emerald-500"
+                  class="font-medium text-emerald-600 hover:underline "
                 >
                   Sign up
                 </Link>
